40ba23045f014fbc516553c45ebbbc35d56f8e0a
lidiajastrzebska <lidiajastrzebska2000@gmail.com>
2021-12-12 16:27:53 +0100

    kickoff new project

---
+++ bulbulator/bulbulator/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bulbulator
+{
+    internal class bulbulator
+    {
+        public int dodawanie(int n, int k)
+        {
+            return n + k;
+        }
+        public int odejmowanie(int n, int k)
+        {
+            return n - k;
+        }
+        public int mnozenie(int n, int k)
+        {
+            return n * k;
+        }
+        public int dzielenie(int n, int k)
+        {
+            return n / k;
+        }
+    }
+}
---
+++ bulbulator/bulbulator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace bulbulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                bulbulator bul1 = new bulbulator();
+                Console.WriteLine();
+                Console.WriteLine("insert two numbers");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int k = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("1. dod");
+                Console.WriteLine("2. odjm");
+                Console.WriteLine("3. mnoz");
+                Console.WriteLine("4. dziel");
+
+                int m = Convert.ToInt32(Console.ReadLine());
+
+                if (m == 1)
+                {
+                    Console.WriteLine(bul1.dodawanie(n,k));
+                }
+                if (m == 2)
+                {
+                    Console.WriteLine(bul1.odejmowanie(n, k));
+                }
+                if (m == 3)
+                {
+                    Console.WriteLine(bul1.mnozenie(n, k));
+                }
+                if (m == 4)
+                {
+                    Console.WriteLine(bul1.dzielenie(n, k));
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
